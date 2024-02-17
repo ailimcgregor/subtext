08780d924e93b96385e96b2cde0a0634cda2ac9f
Syaam Khandaker <syaamkhandaker@gmail.com>
2024-02-17 13:33:22 -0800

    added button prop and updated frontend

---
+++ frontend/src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+type ButtonProps = {
+  text: string;
+  bgColor: string;
+  onClick: () => void;
+};
+
+export default function Button({ text, bgColor, onClick }: ButtonProps) {
+  return (
+    <button
+      className={`rounded-lg ${bgColor} w-full py-4 text-white`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
--- frontend/src/containers/Home/index.tsx
+++ frontend/src/containers/Home/index.tsx
@@ -1,5 +1,6 @@
 import { MutableRefObject, useRef, useState } from "react";
 import Navbar from "../../components/Navbar";
+import Button from "../../components/Button";
 
 export default function Home() {
   const fileRef = useRef<any>();
@@ -12,13 +13,19 @@ export default function Home() {
       {/* <Navbar /> */}
       <div className="flex flex-col items-center">
         <div className="text-2xl font-semibold">Input your file here!</div>
-        <div className="mt-20" onClick={handleClick}>
-          <div className="cursor-pointer border-dotted w-80 h-40 border-2 rounded-lg border-input-field-gray flex justify-center items-center">
+        <div className="mt-10" onClick={handleClick}>
+          <div className="cursor-pointer border-dotted w-96 h-40 border-2 rounded-lg border-input-field-gray flex justify-center items-center">
             <div className="text-sm font-semibold">Input your file here</div>
           </div>
         </div>
       </div>
-      <input type="file" ref={fileRef} hidden />
+      <input className="mt-10" type="file" ref={fileRef} hidden />
+      <div></div>
+      <div className="flex justify-center mt-10">
+        <div className="w-96">
+          <Button text={"Input"} bgColor={"bg-blue-500"} onClick={() => {}} />
+        </div>
+      </div>
     </div>
   );
 }
